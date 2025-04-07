643df75a39fab8398fd22e1e01763120966a8e42
palashkamble <kamblepalash356@gmail.com>
2025-04-07 14:27:55 +0530

    app.jsx changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import UserLayout from './components/Layout/USerLayout'
+import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
 import { Toaster } from 'sonner'
 import Login from './pages/Login'
